b2347054020b38e224a9537256545d730bc73235
yflory <yann.flory@xwiki.com>
2023-01-18 17:31:04 +0100

    Add Express server to receive POST and connect to Gitlab

---
+++ _server/config.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+    httpAddress: '::',
+    httpPort: 3004,
+    gitlab: {
+        url: 'localhost', // localhost will print to the console instead of sending to gitlab
+        token: '',
+        projectID: 0
+    }
+};
